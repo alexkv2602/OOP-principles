898eb01eecd795f31196910782ffc1bf25ac5aff
AlexKV <bloodklesh@gmail.com>
2023-01-10 01:09:44 +0300

    add code

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для номера паспорта
+    public String patronymic; //поле для отчества
+    public String phone; //поле для номера телефона
+    public String surname;//поле для фамилии
+    public boolean subscription;//поле для фамилии
+    public FormDate birthday ; //объект для даты рождения
+
+}
